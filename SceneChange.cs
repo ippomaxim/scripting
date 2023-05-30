b6861174ad9357db84ef554f50f6abf4aa343392
ippomaxim <132684770+ippomaxim@users.noreply.github.com>
2023-05-30 11:26:26 +0300

    Add files via upload

---
+++ SceneChange.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public string sceneName;
+
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        EditorSceneManager.LoadScene(sceneName);
+    }
+
+   
+}
