0ebfb829a3819fcea57294527e870cb06aef6048
ippomaxim <132684770+ippomaxim@users.noreply.github.com>
2023-06-30 09:45:33 +0300

    Add files via upload

---
+++ TemperatureUI.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TemperatureUI : MonoBehaviour
+{
+    public Temperature temperature;
+
+    public TextMeshProUGUI temperatureText; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        float roundTemperature = Mathf.Round(temperature.temperatureCurrent * 10.0f) * 0.1f;
+
+        temperatureText.text = roundTemperature.ToString();
+    }
+}
